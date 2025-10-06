ec9423ad74485daffd11f993ea1115fc717a3104
evgeny <evgst.gl@gmail.com>
2025-10-06 18:00:33 +0700

    png sprites. Relative, absolute paths styles.

--- gulp/images/png.js
+++ gulp/images/png.js
@@ -3,10 +3,13 @@ import * as config from '../config.js';
 import { $ } from '../helper.js';
 const { src, dest } = gulp;
 const { PNG_SPRITE } = config.argvMode;
+let runSprite = true;
 
 const pngSprite = done => {
     if (PNG_SPRITE) {
-        setTimeout(() => {
+        if (runSprite) {
+            runSprite = false;
+
             return src([
                 `${config.imagesPath.spriteSrc}/*.png`
             ])
@@ -14,19 +17,18 @@ const pngSprite = done => {
                     imgName: 'sprite.png',
                     retinaSrcFilter: `${config.imagesPath.spriteSrc}/*@2x.png`,
                     retinaImgName: 'sprite@2x.png',
-                    retinaImgPath: `${config.imagesPath.dist}/sprite`,
+                    retinaImgPath: `${config.imagesPath.dist}`,
                     cssName: '_sprite.scss',
                     algorithm: 'top-down',
                     padding: 2
                 }))
                 .pipe($.debug({ title: 'sprites' }))
-                .pipe(
-                    $.if(/[.]png$/, dest(`${config.imagesPath.dist}/sprite`))
-                )
+                .pipe($.if(/[.]png$/, dest(`${config.imagesPath.src}`)))
+                .pipe($.if(/[.]png$/, dest(`${config.imagesPath.dist}`)))
                 .pipe(
                     $.if(/[.]scss$/, dest(config.imagesPath.spriteStylesDist))
                 );
-        }, 300);
+        }
     }
 
     return done();
